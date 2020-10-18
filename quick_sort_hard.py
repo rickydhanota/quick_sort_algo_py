3a87b4b5923999c5c6150f5394dbc218f879c303
Ricky <rickdhanota@gmail.com>
2020-10-17 22:43:08 -0700

    initial commit

---
+++ quick_sort_hard.py
@@ -0,0 +1,7 @@
+# Quick Sort
+# Write a function that takes in an array of integers and returns a sorted version of that array . Use the quick sort algorithm to sort the array
+# array = [8, 5, 2, 9, 5, 6, 3]
+#output = [2, 3, 5, 5, 6, 8, 9]
+
+def quickSort(array):
+    pass
